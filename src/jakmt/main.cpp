f38a9bba0bbc54cc0a1c953e970f6d84d3b2c220
Jaba Adams <jaba.adams@gmail.com>
2019-02-28 18:08:38 -0500

    Create particle class. Add some timing code.

--- src/jakmt/main.cpp
+++ src/jakmt/main.cpp
@@ -1,11 +1,31 @@
+#include <array>
 #include <iostream>
 #include <memory>
+#include <thread>
 
 
 class Vector3
 {
+	std::array< double, 3 > e;	// elements
+
 public:
-	double e[3];
+	double operator[](int i) const
+	{
+		return e[i];
+	}
+
+	double & operator[](int i)
+	{
+		return e[i];
+	}
+};
+
+
+class Particle
+{
+public:
+	Vector3 position;
+	Vector3 velocity;
 };
 
 
@@ -13,25 +33,56 @@ int main()
 {
 	using namespace std;
 
-	const int NUM_PARTICLES = 35000000;
+	cout << "=====================================================" << endl;
+	cout << "jaKMT - Kinetic Molecular Theory Ideal Gas Simulation" << endl;
+	cout << "=====================================================" << endl;
+	cout << "Number of hardware threads supported: " << thread::hardware_concurrency() << endl;
+	cout << "  NOTE: Currently only using one thread." << endl;
+	cout << endl;
+	cout << "C++ high-resolution clock period: " << chrono::high_resolution_clock::period::num <<
+		" / " << chrono::high_resolution_clock::period::den << endl;
+	cout << "=====================================================" << endl;
+	cout << endl;
 
-	using Vector3ArrayPtr = unique_ptr< Vector3[] >;
+
+	constexpr int ONE_MILLION = 1000000;
+	constexpr int NUM_PARTICLES = 30 * ONE_MILLION;
+
+	using ParticleArrayPtr = unique_ptr< Particle[] >;
 
 	// We want a pre-step and a post-step array.
-	auto particleVectors = make_unique< Vector3ArrayPtr[] >(2);
-	particleVectors[0] = make_unique< Vector3[] >(NUM_PARTICLES);
-	particleVectors[1] = make_unique< Vector3[] >(NUM_PARTICLES);
-	
-	// Fake a time-step
-	for (int i = 0; i < NUM_PARTICLES; ++i)
-	{
-		particleVectors[1][i].e[0] = particleVectors[0][i].e[0] + 0.4242;
-	}
+	cout << "Allocating memory for 2 buffers of " << NUM_PARTICLES << " particles..." << endl;
 
-	// Output some results so that the step isn't optimized away.
-	for (int i = 0; i < 10; ++i)
+	auto start = chrono::high_resolution_clock::now();
+
+	auto particleArrays = make_unique< ParticleArrayPtr[] >(2);
+	particleArrays[0] = make_unique< Particle[] >(NUM_PARTICLES);
+	particleArrays[1] = make_unique< Particle[] >(NUM_PARTICLES);
+	
+	std::chrono::duration< double > elapsedSeconds = chrono::high_resolution_clock::now() - start;
+
+	cout << "Finished allocating memory." << endl;
+	cout << "Allocation took " << elapsedSeconds.count() << " seconds." << endl;
+	cout << endl;
+
+	for (auto t = 0; t < 3; ++t)
 	{
-		cout << particleVectors[1][i].e[0] << endl;
+		// Fake a time-step
+		cout << "Stepping particles..." << endl;
+		
+		start = chrono::high_resolution_clock::now();	
+		for (int i = 0; i < NUM_PARTICLES; ++i)
+		{
+			particleArrays[1][i].position[0] = particleArrays[0][i].position[0] + 0.4242;
+		}
+		elapsedSeconds = chrono::high_resolution_clock::now() - start;
+		cout << "Particle step took " << elapsedSeconds.count() << " seconds." << endl;
+
+		// Output some results so that the step isn't optimized away.
+		for (int i = 0; i < 2; ++i)
+		{
+			cout << particleArrays[1][i].position[0] << endl;
+		}	
 	}
 	return 0;
 }
