66003f091b827b7bae46ea7bd4108816b007d668
Jaba Adams <jaba.adams@gmail.com>
2019-02-28 19:14:23 -0500

    Implemented threading, but no real performance increase. Will have to think about thread creation overhead.

--- src/jakmt/main.cpp
+++ src/jakmt/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include <thread>
+#include <vector>
 
 
 class Vector3
@@ -29,6 +30,23 @@ public:
 };
 
 
+using ParticleArrayPtr = std::unique_ptr< Particle[] >;
+auto g_particleArrays = std::make_unique< ParticleArrayPtr[] >(2);
+
+
+void stepParticles(int iFirst, int iLast)
+{
+	using namespace std;
+
+	// cout << iFirst << " " << iLast << endl;
+	// cout << g_particleArrays.get() << endl;
+	for (int i = iFirst; i <= iLast; ++i)
+	{
+		g_particleArrays[1][i].position[0] = g_particleArrays[0][i].position[0] + 0.4242;
+	}
+}
+
+
 int main()
 {
 	using namespace std;
@@ -48,16 +66,13 @@ int main()
 	constexpr int ONE_MILLION = 1000000;
 	constexpr int NUM_PARTICLES = 30 * ONE_MILLION;
 
-	using ParticleArrayPtr = unique_ptr< Particle[] >;
-
 	// We want a pre-step and a post-step array.
 	cout << "Allocating memory for 2 buffers of " << NUM_PARTICLES << " particles..." << endl;
 
 	auto start = chrono::high_resolution_clock::now();
 
-	auto particleArrays = make_unique< ParticleArrayPtr[] >(2);
-	particleArrays[0] = make_unique< Particle[] >(NUM_PARTICLES);
-	particleArrays[1] = make_unique< Particle[] >(NUM_PARTICLES);
+	g_particleArrays[0] = make_unique< Particle[] >(NUM_PARTICLES);
+	g_particleArrays[1] = make_unique< Particle[] >(NUM_PARTICLES);
 	
 	std::chrono::duration< double > elapsedSeconds = chrono::high_resolution_clock::now() - start;
 
@@ -65,23 +80,41 @@ int main()
 	cout << "Allocation took " << elapsedSeconds.count() << " seconds." << endl;
 	cout << endl;
 
-	for (auto t = 0; t < 3; ++t)
+	for (auto s = 0; s < 3; ++s)
 	{
 		// Fake a time-step
 		cout << "Stepping particles..." << endl;
 		
+		constexpr int NUM_THREADS = 4;
+		assert( NUM_PARTICLES >= NUM_THREADS );
+		int particlesPerThread = NUM_PARTICLES / NUM_THREADS;
+
+		auto iFirstParticle = 0;
+		auto threads = make_unique< unique_ptr< thread >[] >(NUM_THREADS);
+
 		start = chrono::high_resolution_clock::now();	
-		for (int i = 0; i < NUM_PARTICLES; ++i)
+		for (int iThread = 0; iThread < NUM_THREADS; ++iThread)
 		{
-			particleArrays[1][i].position[0] = particleArrays[0][i].position[0] + 0.4242;
+			auto iLastParticle = min( iFirstParticle + particlesPerThread-1, NUM_PARTICLES-1 );
+			// cout << iThread << " : " << iFirstParticle << " -> " << iLastParticle << endl;
+			
+			threads[iThread] = make_unique< thread >(thread(stepParticles, iFirstParticle, iLastParticle));
+
+			iFirstParticle += particlesPerThread;
 		}
+
+		for (int iThread = 0; iThread < NUM_THREADS; ++iThread)
+		{
+			threads[iThread]->join();
+		}
+
 		elapsedSeconds = chrono::high_resolution_clock::now() - start;
 		cout << "Particle step took " << elapsedSeconds.count() << " seconds." << endl;
 
 		// Output some results so that the step isn't optimized away.
 		for (int i = 0; i < 2; ++i)
 		{
-			cout << particleArrays[1][i].position[0] << endl;
+			cout << g_particleArrays[1][i].position[0] << endl;
 		}	
 	}
 	return 0;
