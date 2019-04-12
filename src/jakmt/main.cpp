e077f78f6696adae9943aba4ed96f619962e4b6e
Jaba Adams <jaba.adams@gmail.com>
2019-04-12 09:50:37 -0400

    Randomly initializing speed.

--- src/jakmt/main.cpp
+++ src/jakmt/main.cpp
@@ -50,24 +50,49 @@ int main()
 	cout << "Allocation took " << elapsedSeconds.count() << " seconds." << endl;
 	cout << endl;
 
-	auto TIME_STEP = 1e-9;	// seconds
-	cout << "dt = " << TIME_STEP << endl;
+	// Initialize particle positions and velocities...
 
-	// Initialize particle positions...
+	// We're going to measure time in nanoseconds. So one time unit will be one ns.
+	// We're going to measure position in terms of the size of the simulation box. One box width
+	// will be 1 length unit.
+	// We're going to measure speed in box widths / nanosecond. So a speed of 1 means that
+	// the particle goes BOX_SIDE meters per 1e-9 seconds.
+
+	constexpr auto  NS_PER_S = 1e9;	// nanoseconds (ns) per second (s)
+	constexpr auto  TIME_STEP = 1;	// nanoseconds (ns)
+	cout << "dt (s) = " << 1 / NS_PER_S << endl;
+
+	constexpr auto BOX_SIDE = 1e-6;	// m
+	cout << "Simulation box side (m): " << BOX_SIDE << endl;
+
+	constexpr auto INITIAL_SPEED_COORDINATE_MPS = 500;	// m/s
+	constexpr auto  SCALED_SPEED = INITIAL_SPEED_COORDINATE_MPS / BOX_SIDE / NS_PER_S;
+	cout << "Initial speed coordinate (m/s): " << INITIAL_SPEED_COORDINATE_MPS << endl;
+	cout << "Initial speed coordinate (box width / step): " << SCALED_SPEED << endl;
+
 	random_device rd;
 	mt19937 rand_gen(rd());	// Mersenne Twister
 	uniform_real_distribution<> rand_coordinate(-0.5, 0.5);
+	uniform_real_distribution<> rand_speed(-SCALED_SPEED, SCALED_SPEED);	// m/s
+
 
 	for (auto i = 0; i < NUM_PARTICLES; ++i)
 	{
 		particleArrays[0][i].position[0] = rand_coordinate(rand_gen);
 		particleArrays[0][i].position[1] = rand_coordinate(rand_gen);
 		particleArrays[0][i].position[2] = rand_coordinate(rand_gen);
+
+		particleArrays[0][i].velocity[0] = rand_speed(rand_gen);
+		particleArrays[0][i].velocity[1] = rand_speed(rand_gen);
+		particleArrays[0][i].velocity[2] = rand_speed(rand_gen);		
 	}
 
 	cout << particleArrays[0][0].position << endl;	
 
-	for (auto t = 0; t < 2; ++t)
+	auto src = 0;
+	auto dst = (src + 1) % 2;
+
+	for (auto t = 0; t < 4; ++t)
 	{
 		// Fake a time-step
 		cout << "Stepping particles..." << endl;
@@ -75,16 +100,22 @@ int main()
 		start = chrono::high_resolution_clock::now();	
 		for (int i = 0; i < NUM_PARTICLES; ++i)
 		{
-			particleArrays[1][i].position = particleArrays[0][i].position + TIME_STEP * particleArrays[0][i].velocity;
+			particleArrays[dst][i].position = particleArrays[src][i].position + TIME_STEP * particleArrays[src][i].velocity;
+			particleArrays[dst][i].velocity = particleArrays[src][i].velocity;
 		}
 		elapsedSeconds = chrono::high_resolution_clock::now() - start;
 		cout << "Particle step took " << elapsedSeconds.count() << " seconds." << endl;
 
 		// Output some results so that the step isn't optimized away.
-		for (int i = 0; i < 2; ++i)
+		for (int i = 0; i < 1; ++i)
 		{
-			cout << particleArrays[1][i].position << endl;
-		}	
+			cout << particleArrays[dst][i].position << endl;
+		}
+
+		// Swap source and destination arrays...
+		src = (src + 1) % 2;
+		dst = (dst + 1) % 2;
+		assert(src != dst);
 	}
 	return 0;
 }
